2b98df012b8a4bb13c2d01eccb286156df87beb4
Sean B <strawhorse@gmail.com>
2025-05-22 14:03:34 +0100

    Can use a lambda function to pass the output of the helper class as a parameter for the other method

--- src/ie/atu/sw/Encodings.java
+++ src/ie/atu/sw/Encodings.java
@@ -10,6 +10,16 @@ public class Encodings {
 //    One to hold the text words/suffixes and the other to hold the numeric values
 //    need to find size of the arrays first before instantiating them
 
+//    Could create a helper method which runs a count on the csv file first and then sends the values to this method
+
+    // pass method2 as argument to method1
+//    public void method1(method2());
+
+//    OR
+
+//    obj.square(obj.add(15, 9));
+
+
     static void outputEncodings() throws FileNotFoundException {
 
 
