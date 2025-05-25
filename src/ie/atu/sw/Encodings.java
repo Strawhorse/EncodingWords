0ad32ae9db32975995f66d3e36bb2deaa9fb4c80
John Bracken <strawhorse@gmail.com>
2025-05-25 20:16:40 +0100

    Fixed parsing issue of CSV file
    Now have the encodings and the endings in two separate arrays which can be iterated over

--- src/ie/atu/sw/Encodings.java
+++ src/ie/atu/sw/Encodings.java
@@ -2,34 +2,44 @@ package ie.atu.sw;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Encodings {
 
-//    test out reading in the encodings to two separate arrays
-//    One to hold the text words/suffixes and the other to hold the numeric values
-//    need to find size of the arrays first before instantiating them
-
-//    Could create a helper method which runs a count on the csv file first and then sends the values to this method
-
-    // pass method2 as argument to method1
-//    public void method1(method2());
-
-//    OR
-
-//    obj.square(obj.add(15, 9));
-
-
     static void outputEncodings() throws FileNotFoundException {
-
-
 
         File myObj = new File("src/encodings-10000.csv");
         Scanner myReader = new Scanner(myObj);
+
+//        create arraylists with more functionality than normal arrays; can convert back later
+        List<String> endings = new ArrayList<>();
+        List<String> encodings = new ArrayList<>();
+
         while (myReader.hasNextLine()) {
             String data = myReader.nextLine();
-            System.out.println(data);
+            String[] values = data.split(",");
+
+//            separate the endings and the encodings into different arraylists
+            for (int i = 0; i < values.length; i++) {
+                if (i % 2 == 0) {
+                    endings.add(values[i]);
+                } else {
+                    encodings.add(values[i]);
+                }
+            }
         }
-        myReader.close();
+
+        myReader.close();  // Close the scanner
+
+        // Convert lists to arrays
+        String[] endingsArray = endings.toArray(new String[0]);
+        String[] encodingsArray = encodings.toArray(new String[0]);
+
+        // Print the arrays to test
+        System.out.println(Arrays.toString(endingsArray));
+        System.out.println(Arrays.toString(encodingsArray));
     }
 }
